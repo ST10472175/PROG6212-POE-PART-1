83a0c9481a4977d76261b94133422e0b2e3078fa
Gift Ramavhinyana <st10472175@rcconnect.edu.za>
2026-09-04 07:20:33 +0200

    Document database table creation

--- PROG POE.sql
+++ PROG POE.sql
@@ -1,7 +1,7 @@
 --Raceday Event Management System Database--
 create database RaceDay;
 use RaceDay;
-
+--create database tables for the raceday system--
 create table EVENTS(event_ID varchar(10) primary key,
                     event_Name varchar(50),
                     event_Date varchar(25),
